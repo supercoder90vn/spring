4942c9a55fd996414f6fd7a10e49be443eec72ab
ho ngoc phuc <hongocphuc90dn@gmail.com>
2016-07-26 19:19:45 +0700

    update

---
+++ sql/data_editor.sql
@@ -0,0 +1,7 @@
+delete from spring.messages;
+delete from spring.offers;
+delete from spring.users;
+
+delete from spring_test.messages;
+delete from spring_test.offers;
+delete from spring_test.users;
--- sql/data_test.sql
+++ sql/data_test.sql
@@ -15,20 +15,6 @@ CREATE SCHEMA IF NOT EXISTS `spring_test` DEFAULT CHARACTER SET utf8 COLLATE utf
 USE `spring_test` ;
 
 -- -----------------------------------------------------
--- Table `spring_test`.`offers`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `spring_test`.`offers` ;
-
-CREATE TABLE IF NOT EXISTS `spring_test`.`offers` (
-  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
-  `name` VARCHAR(100) NOT NULL COMMENT '',
-  `email` VARCHAR(60) NOT NULL COMMENT '',
-  `text` TEXT NOT NULL COMMENT '',
-  PRIMARY KEY (`id`)  COMMENT '')
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `spring_test`.`users`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `spring_test`.`users` ;
@@ -36,20 +22,52 @@ DROP TABLE IF EXISTS `spring_test`.`users` ;
 CREATE TABLE IF NOT EXISTS `spring_test`.`users` (
   `username` VARCHAR(60) NOT NULL COMMENT '',
   `password` VARCHAR(80) NULL COMMENT '',
+  `authority` VARCHAR(45) NULL COMMENT '',
+  `name` VARCHAR(100) NOT NULL COMMENT '',
   `enabled` TINYINT(1) NULL DEFAULT 1 COMMENT '',
+  `email` VARCHAR(45) NOT NULL COMMENT '',
   PRIMARY KEY (`username`)  COMMENT '')
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `spring_test`.`authorities`
+-- Table `spring_test`.`offers`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `spring_test`.`authorities` ;
+DROP TABLE IF EXISTS `spring_test`.`offers` ;
 
-CREATE TABLE IF NOT EXISTS `spring_test`.`authorities` (
+CREATE TABLE IF NOT EXISTS `spring_test`.`offers` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `text` TEXT NOT NULL COMMENT '',
   `username` VARCHAR(60) NOT NULL COMMENT '',
-  `authority` VARCHAR(45) NULL COMMENT '',
-  PRIMARY KEY (`username`)  COMMENT '')
+  PRIMARY KEY (`id`, `username`)  COMMENT '',
+  INDEX `fk_offers_users_idx` (`username` ASC)  COMMENT '',
+  CONSTRAINT `fk_offers_users`
+    FOREIGN KEY (`username`)
+    REFERENCES `spring_test`.`users` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `spring_test`.`messages`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `spring_test`.`messages` ;
+
+CREATE TABLE IF NOT EXISTS `spring_test`.`messages` (
+  `id` INT NOT NULL AUTO_INCREMENT COMMENT '',
+  `subject` VARCHAR(100) NOT NULL COMMENT '',
+  `content` VARCHAR(1000) NOT NULL COMMENT '',
+  `name` VARCHAR(100) NOT NULL COMMENT '',
+  `email` VARCHAR(60) NOT NULL COMMENT '',
+  `username` VARCHAR(60) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`)  COMMENT '',
+  INDEX `fk_messages_users1_idx` (`username` ASC)  COMMENT '',
+  CONSTRAINT `fk_messages_users1`
+    FOREIGN KEY (`username`)
+    REFERENCES `spring_test`.`users` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
